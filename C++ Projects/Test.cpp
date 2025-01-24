8a7b713b93c6e1c083cb9387f433e90846bb4ad0
George Brea <gbrea@optonline.net>
2025-01-24 13:19:44 -0500

    get the py

--- C++ Projects/Test.cpp
+++ C++ Projects/Test.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+string name;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name <<"!!" << endl;
     return 0;
 }
